b15b6bb1e121afc6e86650a9227404714f79c3e5
Mina <mina.r.meshreky@gmail.com>
2025-08-27 14:17:44 -0400

    UI:adding NavLink component for navigation

---
+++ resources/js/Components/UI/NavLink.vue
@@ -0,0 +1,29 @@
+<script setup>
+import {computed, defineProps} from 'vue';
+import {usePage} from "@inertiajs/vue3";
+
+const props = defineProps({
+    routeName: {
+        type: String,
+        required: true
+    },
+    componentName: {
+        type: String,
+        required: true
+    }
+});
+
+const page = usePage()
+
+const isActiveComponent = computed(() => page.component === props.componentName)
+</script>
+
+<template>
+    <Link
+        :href="route(props.routeName)"
+        class="px-3 py-2 rounded-md hover:bg-slate-700"
+        :class="{'bg-slate-700 pointer-events-none': isActiveComponent}"
+    >
+        <slot />
+    </Link>
+</template>
