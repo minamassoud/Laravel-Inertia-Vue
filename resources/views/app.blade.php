d791a1f82f63fd43587b6f3b1f181837d1ca3a0d
Mina <mina.r.meshreky@gmail.com>
2025-08-27 17:04:33 -0400

    UI: update body text color for light mode consistency

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,13 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    
+
     @vite('resources/js/app.js')
     @inertiaHead
     @routes
 </head>
 
-<body class="font-Montserrat bg-slate-100 text-slate-800 dark:bg-slate-800 dark:text-slate-100">
+<body class="font-Montserrat bg-slate-100 text-slate-700 dark:bg-slate-700 dark:text-slate-100">
     @inertia
 </body>
 
