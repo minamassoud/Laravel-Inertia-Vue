126343c8054e665eb47217ad7d2aea149a40f657
Mina <mina.r.meshreky@gmail.com>
2025-09-24 14:09:32 -0400

    "Feature: display paginated listings on Home with reusable Card component
    Added `Card` component for rendering individual listing details. Updated `ListingController@index` to fetch paginated listings with user data. Modified `Home.vue` to use the new component and display listings in a responsive grid layout. Extended Tailwind CSS with a 3-column grid utility."

---
+++ resources/js/Components/Card.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+const props = defineProps({
+    listing: Object
+})
+
+</script>
+
+<template>
+    <div class="rounded shadow-lg bg-gray-200 dark:bg-slate-800">
+        <div class="bg-slate-500">
+            <img :src="listing.image ? `/storage/${listing.image}` : '/storage/default/listing/default_img.jpg'"
+                 alt="image" class="w-full h-48 object-cover object-center">
+        </div>
+        <div class="p-4 flex flex-col gap-4">
+            <p class="font-bold">
+                {{ listing.title.substring(0, 40) }}{{ listing.title.length > 40 ? '...' : '.' }}
+            </p>
+            <p>
+                Listed on {{ new Date(listing.created_at).toLocaleDateString() }} by
+                <button class="text-link">
+                    {{ listing.user.name }}
+                </button>
+            </p>
+            <div class="flex flex-row flex-wrap gap-3">
+                <span v-for="tag in listing.tags.split(',')" :key="tag" class="pullet-btn cursor-pointer">
+                    {{ tag }}
+                </span>
+            </div>
+
+        </div>
+
+    </div>
+</template>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,10 +1,24 @@
 <script setup>
 import {Head} from "@inertiajs/vue3";
+import Card from "../Components/Card.vue";
+
+
+const props = defineProps({
+    listings: Object
+})
+
+console.log(props.listings)
 </script>
 
 <template>
-    <head title="- home"/>
-    <header>
-        <p>This is the home page.</p>
-    </header>
+    <head title="Listings"/>
+
+    <div v-if="Object.keys(listings.data).length">
+        <div class="grid grid-col-3 gap-4">
+            <div v-for="listing in props.listings.data" :key="listing.id">
+                <Card :listing="listing"/>
+            </div>
+        </div>
+    </div>
+
 </template>
